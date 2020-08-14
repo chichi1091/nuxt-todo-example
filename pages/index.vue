e876f384e34bcdd9d50597f83b6716adadb236b4
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2020-08-14 20:52:20 +0900

    サーバとのつなぎ込み以外を実装

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,34 @@
         :sort-desc="true"
         class="elevation-1"
       >
+        <template v-slot:top>
+          <v-dialog v-model="dialog" max-width="500px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">TODO編集</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field v-model="todo.task" label="タスク" />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn @click="close">
+                  閉じる
+                </v-btn>
+                <v-btn class="primary" @click="update">
+                  更新する
+                </v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
         <template v-slot:[`item.actions`]="{ item }">
           <v-icon
             small
@@ -52,12 +80,32 @@ export default {
         { text: 'ID', value: 'id' },
         { text: 'タスク', value: 'task' },
         { text: '操作', value: 'actions' }
-      ]
+      ],
+      todo: {}
     }
   },
   computed: {
     todos () {
       return this.$store.getters.getTodos
+    }
+  },
+  methods: {
+    edit (todo) {
+      this.todo = Object.assign({}, todo)
+      this.dialog = true
+    },
+    update () {
+      const payload = { todo: this.todo }
+      this.$store.commit('updateTodo', payload)
+      this.close()
+    },
+    remove (todo) {
+      const payload = { todo }
+      this.$store.commit('removeTodo', payload)
+    },
+    close () {
+      this.dialog = false
+      this.todo = {}
     }
   }
 }
